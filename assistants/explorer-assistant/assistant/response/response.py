2a640f8d4074973ec16e239a2cf78445b958591b
Brian Krabach <brkrabac@microsoft.com>
2025-01-10 21:30:05 +0000

    clean up utility imports

--- assistants/explorer-assistant/assistant/response/response.py
+++ assistants/explorer-assistant/assistant/response/response.py
@@ -21,18 +21,17 @@ from semantic_workbench_api_model.workbench_model import (
 )
 from semantic_workbench_assistant.assistant_app import ConversationContext
 
-from assistant.response.utils.message_utils import build_system_message_content
-from assistant.response.utils.response_provider_utils import initialize_response_provider
-
 from ..config import AssistantConfigModel
 from .tool_handler import parse_tool_response
 from .utils import (
+    build_system_message_content,
     conversation_message_to_completion_messages,
     establish_mcp_sessions,
     get_history_messages,
     get_response_duration_message,
     get_token_usage_message,
     handle_tool_action,
+    initialize_response_provider,
     inject_attachments_inline,
     num_tokens_from_messages,
     retrieve_tools_from_sessions,
--- assistants/explorer-assistant/assistant/response/utils/__init__.py
+++ assistants/explorer-assistant/assistant/response/utils/__init__.py
@@ -1,20 +1,24 @@
 from .formatting_utils import get_response_duration_message, get_token_usage_message
 from .mcp_server_utils import establish_mcp_sessions
 from .message_utils import (
+    build_system_message_content,
     conversation_message_to_completion_messages,
     get_history_messages,
     inject_attachments_inline,
 )
+from .response_provider_utils import initialize_response_provider
 from .token_utils import num_tokens_from_messages
 from .tool_utils import handle_tool_action, retrieve_tools_from_sessions
 
 __all__ = [
+    "build_system_message_content",
     "conversation_message_to_completion_messages",
     "establish_mcp_sessions",
     "get_history_messages",
     "get_response_duration_message",
     "get_token_usage_message",
     "handle_tool_action",
+    "initialize_response_provider",
     "inject_attachments_inline",
     "num_tokens_from_messages",
     "retrieve_tools_from_sessions",
